6861e11e57659c59006c32e2c092275f33defed7
Chrisfish <806247668@qq.com>
2019-09-15 22:08:03 +0800

    circlequeue

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -87,28 +87,100 @@ void print1(QueueList front, QueueList rear) {
 }
 
 //	应用： 主机与外部用户资源不匹配  多用户资源竞争 离散事件模拟 图的广度优先搜索 树的层次遍历
+
+//	循环队列: front和rear中间有一块空的存储单元
+typedef struct circleQueue {
+	int queue[M];
+	int front;
+	int rear;
+}CQueue;
+
+void init3(CQueue* cq) {
+	cq->front = cq->rear = 0;
+}
+
+bool isEmpty3(CQueue* cq) {
+	return cq->front == cq->rear;
+}
+
+bool isFull3(CQueue* cq) {
+	return (cq->rear + 1) % M == cq->front;
+}
+
+bool push3(CQueue* cq, int data) {
+	if (isFull3(cq))return false;
+	cq->queue[cq->rear] = data;
+	cq->rear = (cq->rear + 1) % M;
+	return true;
+}
+
+bool pop3(CQueue* cq, int &data) {
+	if (isEmpty3(cq))return false;
+	data = cq->queue[cq->front];
+	cq->front = (cq->front + 1) % M;
+	return true;
+}
+
+bool print3(CQueue* cq) {
+	if(isEmpty3(cq))return false;
+	if (cq->front < cq->rear) {
+		for (int i = cq->front; i < cq->rear; i++) {
+			std::cout << cq->queue[i] << " ";
+		}
+	}
+	else {
+		for (int i = cq->front; i < cq->rear + M; i++) {
+			std::cout << cq->queue[i] << " ";
+		}
+	}
+	return true;
+}
 int main()
 {
-	int n;/*
-	init(front, rear);
-	push(queue, front, rear, 22);
-	push(queue, front, rear, 22);
-	push(queue, front, rear, 33);
-	push(queue, front, rear, 20);
-	pop(queue, front, rear, n);
-	print(queue, front, rear);
-    std::cout << "Hello World!\n" << n;
-	*/
-	//	2
-	QueueList p = NULL, q = NULL;
-	init(p, q);
-	push(p, q, 1);
-	push(p, q, 2);
-	push(p, q, 3);
-	pop(p, q, n);
-	delet(p, q);
-	print1(p, q);
-	std::cout << "Hello World!\n" << n;
+	////	1
+	//int n;
+	//init(front, rear);
+	//push(queue, front, rear, 22);
+	//push(queue, front, rear, 22);
+	//push(queue, front, rear, 33);
+	//push(queue, front, rear, 20);
+	//pop(queue, front, rear, n);
+	//print(queue, front, rear);
+ //   std::cout << "Hello World!\n" << n;
+	//
+	////	2
+	//QueueList p = NULL, q = NULL;
+	//init(p, q);
+	//push(p, q, 1);
+	//push(p, q, 2);
+	//push(p, q, 3);
+	//pop(p, q, n);
+	//delet(p, q);
+	//print1(p, q);
+	//std::cout << "Hello World!\n" << n;
+	////	3
+	//CQueue q;
+	//
+	//int a = 0;
+	//init3(&q);
+	//push3(&q, 22);
+	//push3(&q, 21);
+	//push3(&q, 23);
+	//print3(&q);
+
+	//pop3(&q, a);
+	//std::cout << a;
+	//print3(&q);
+
+	CQueue* q = NULL;
+
+	int a = 0;
+	init3(q);
+	push3(q, 22);
+	push3(q, 21);
+	push3(q, 23);
+	print3(q);
+
 }
 
 
