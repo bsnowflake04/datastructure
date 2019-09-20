d230f832f05d23bcd9af6709ffae5357b6634d30
Chrisfish <806247668@qq.com>
2019-09-20 19:03:51 +0800

    finished tree

--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "Tree.h"
 #define M 30
 
 typedef struct node {
@@ -10,7 +11,7 @@ typedef struct node {
 }BTnode, *BTree;
 
 void visit(BTree b) {
-	;
+	
 }
 //	遍历二叉树的递归算法：前，中，后序遍历
 void preorder(BTree b) {
@@ -68,21 +69,22 @@ void preorder2(BTree b) {
 }
 void inorder2(BTree b) {
 	BTree stack[M] = { NULL }, p = b;
-	int top = -1;
+	int top = -1;//	LinkList top = NULL;
 	if (p != NULL) {
 		do {
 			while (p != NULL) {
-				stack[++top] = p;
+				stack[++top] = p;//	q = (Btree)malloc(sizeof(BTnode)); q->data = p; q->link = top; top = p;
 				p = p->lchild;
 			}
-			p = stack[top--];
+			p = stack[top--];//	p = top->data; q = top; top = top->link; free(q);
 			visit(p);
 			p = p->rchild;
 		} while (!(top == -1 && p == NULL));
 	}
 }
+
 //	???错误
-void inorder3(BTree b) {
+void postorder3(BTree b) {
 	BTree stack[M] = { NULL }, p = b;
 	int top = -1;
 	if (p != NULL) {
@@ -215,7 +217,140 @@ int game1_1(BTree t) {
 
 //	已知具有n个结点的非空完全二叉树采用顺序存储结构，结点的数据信息依次存放于数组BT[0..n-1],请写出产生该二叉树二叉链表的算法
 #define maxnode 100
-BTree build
+BTree game2(int bt[], int n) {
+	BTree t, ptr[maxnode];
+
+	ptr[0] = (BTree)malloc(sizeof(BTnode));
+	ptr[0]->data = bt[0];
+	ptr[0]->lchild = NULL;
+	ptr[0]->rchild = NULL;
+
+	t = ptr[0];
+	for (int i = 1; i < n; i++) {
+		//if (bt[i] == NULL)continue;//	非空二叉树
+		ptr[i] = (BTree)malloc(sizeof(BTnode));
+		ptr[i]->data = bt[i];
+		ptr[i]->lchild = NULL;
+		ptr[i]->rchild = NULL;
+		int j = (i - 1) / 2;
+		if (i - 2 * j - 1 == 0)ptr[j]->lchild = ptr[i];
+		else ptr[j]->rchild = ptr[i];
+	}
+	return t;
+}
+
+//	已知两二叉树采用二叉链表存储结构，根节点指针分别为t1,t2,写一递归算法，判断这两颗二叉树是否等价
+bool game3(BTree t1, BTree t2) {
+	if (t1 == NULL && t2 == NULL)return true;
+	if (t1 != NULL && t2 != NULL && t1->data == t2->data && game3(t1->lchild, t2->lchild) && game3(t1->rchild, t2->rchild))return true;
+	return false;
+}
+
+//	已知二叉树采用二叉链表存储结构，根节点地址为t，写一算法删除数据域内容为item的结点以及以该节点为根的子树上的所有结点(查找(前序遍历)，删除)
+void release(BTree& p) {
+	if (p != NULL) {
+		release(p->lchild);
+		release(p->rchild);
+		free(p);
+	}
+}
+BTree game4(BTree t, int item){
+	BTree stack[M] = { NULL }, p = t, q = NULL;
+	int top = -1;
+	if (t != NULL) {
+		do {
+			while (p != NULL) {
+				if (p->data == item) {
+					if (p == t)t = NULL;
+					if (q->lchild == p)q->lchild = NULL;
+					if (q->rchild == p)q->rchild = NULL;
+					release(p);
+					return t;
+				}
+				stack[++top] = p;
+				q = p;
+				p = p->lchild;
+			}
+			p = stack[top--];
+			q = p;
+			p = p->rchild;
+		} while (!(p == NULL && top == -1));
+	}
+}
+
+//	已知非空二叉树采用二叉链表存储结构，根节点指针为t，写一复制该二叉树的算法
+BTree game5(BTree t) {
+	BTree p;
+	if (t == NULL)return NULL;
+	else {
+		p = (BTree)malloc(sizeof(BTnode));
+		p->lchild = game5(t->lchild);
+		p->rchild = game5(t->rchild);
+		p->data = t->data;
+	}
+	return p;
+}
+
+//	已知二叉树采用二叉链表存储结构，根节点指针为t，请写一算法判断该二叉树是否为二叉排序树
+bool game6(BTree t) {
+	BTree stack[M], p = t;
+	int top = -1, x = 0;
+	if (t != NULL) {
+		do {
+			while (p != NULL) {
+				stack[++top] = p;
+				p = p->lchild;
+			}
+			p = stack[top--];
+			if (p->data < x)return false;
+			x = p->data;
+			p = p->rchild;
+		} while (!(top == -1 && p == NULL));
+	}
+	return true;
+}
+
+//	已知二叉树的前序遍历序列与中序遍历序列存放于数组preor[n],与inor[n]中，并且各结点数据信息均不相同，写一算法生成该二叉树的二叉链表存储结构
+//	求前序序列中结点在中序序列中的位置，每个结点的位置值相当于data值？类似一种权重？
+
+int searchpos(int inor[], int n, int item) {
+	for (int i = 0; i < n; i++) {
+		if (inor[i] == item)return i + 1;
+	}
+}
+
+void insertitem(BTree& t, int inor[], int n, int item) {//	二叉有序树中插入一个结点
+	BTree p = NULL, q = t;
+	int ord;
+	p = (BTree)malloc(sizeof(BTnode));
+	p->data = item;
+	p->lchild = p->rchild = NULL;
+	if (t == NULL)p = t;
+	else {
+		ord = searchpos(inor, n, item);
+		while (true) {
+			if (ord < searchpos(inor, n, q->data)) {
+				if (q->lchild != NULL)q = q->lchild;
+				else {
+					q->lchild = p; break;
+				}
+			}
+			else {
+				if (q->rchild != NULL)q = q->rchild;
+				else {
+					q->rchild = p; break;
+				}
+			}
+		}
+	}
+}
+BTree game7(int inor[], int n, int preod[]) {
+	BTree t = NULL;
+	for (int i = 0; i < n; i++) {
+		insertitem(t, inor, n, preod[i]);
+	}
+	return t;
+}
 int main()
 {
     std::cout << "Hello World!\n";
